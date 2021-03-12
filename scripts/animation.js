32f29de526595aecb266129684b7af4efa8e95e4
Ricky Chon <rickychon99@gmail.com>
2021-03-12 02:38:22 -0500

    Complete animation lab

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,14 +1,54 @@
-const canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', init);
+
+let canvas, ctx, img, button, frames, count;
 
 function init()
 {
-    window.requestAnimationFrame(draw);
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    img = new Image();
+    img.src = 'assets/images/numbers.png';
+
+    frames = numbers.frames;
+    drawNumber([0]);
+
+    button = document.getElementById('start');
+    button.addEventListener('click', startAnim);
 }
 
-function draw()
+function startAnim()
 {
-    window.requestAnimationFrame(draw);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    count = 0;
+
+    button.disabled = true;
+    let timer = setInterval(() =>
+    {
+        if(count >= frames.length)
+        {
+            clearInterval(timer);
+            button.disabled = false;
+        }
+        drawNumber(count.toString().split('').map(Number));
+        count++;
+    }, 500);
 }
 
-init();
+function drawNumber(numbers = [])
+{
+    numbers.reverse().forEach((num, idx) =>
+    {
+        let fNum = frames[num];
+        ctx.drawImage(
+            img,
+            fNum.frame.x,
+            fNum.frame.y,
+            fNum.frame.w,
+            fNum.frame.h,
+            numbers.length > 1 ? canvas.width / 2 - (fNum.frame.w * idx) : canvas.width / 4,
+            0,
+            fNum.sourceSize.w,
+            fNum.sourceSize.h);
+    });
+}
